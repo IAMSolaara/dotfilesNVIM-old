7abe48677a3d115403c61e2fae06036d4a0306d8
Lorenzo Cauli <lorenzocauli162@gmail.com>
2022-11-29 09:05:13 +0100

    switch coc for nvim LSP

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -127,19 +127,19 @@ require('packer').startup(function(use)
 	use 'pangloss/vim-javascript'
 	use 'MaxMEllon/vim-jsx-pretty'
 
-	-- misc plugins --lorecast162
-	use {'neoclide/coc.nvim', branch = 'release', config = function()
-		local utils = require("utils")
-		-- CoC settings --lorecast162
-		vim.g.coc_global_extensions = {'coc-tsserver', 'coc-rust-analyzer', 'coc-emmet', 'coc-clangd', 'coc-pyright', 'coc-json', 'coc-go', 'coc-java', 'coc-flutter', 'coc-html', 'coc-lua'}
-
-		-- use prettier if it's installed --lorecast162
-		if vim.fn.isdirectory('./node_modules') and vim.fn.isdirectory('./node_modules/prettier')
-		then
-			table.insert(vim.g.coc_global_extensions, "coc-prettier")
-		end
-		utils.map("i", "<cr>", "coc#pum#visible() ? coc#pum#confirm() : \"\\<CR>\"", {expr = true})
-	end}
+	--	-- misc plugins --lorecast162
+	--	use {'neoclide/coc.nvim', branch = 'release', config = function()
+	--		local utils = require("utils")
+	--		-- CoC settings --lorecast162
+	--		vim.g.coc_global_extensions = {'coc-tsserver', 'coc-rust-analyzer', 'coc-emmet', 'coc-clangd', 'coc-pyright', 'coc-json', 'coc-go', 'coc-java', 'coc-flutter', 'coc-html', 'coc-lua'}
+	--
+	--		-- use prettier if it's installed --lorecast162
+	--		if vim.fn.isdirectory('./node_modules') and vim.fn.isdirectory('./node_modules/prettier')
+	--		then
+	--			table.insert(vim.g.coc_global_extensions, "coc-prettier")
+	--		end
+	--		utils.map("i", "<cr>", "coc#pum#visible() ? coc#pum#confirm() : \"\\<CR>\"", {expr = true})
+	--	end}
 
 	use {'airblade/vim-gitgutter', config = function()
 		local utils = require("utils")
@@ -208,4 +208,100 @@ require('packer').startup(function(use)
 		-- Vista binds --lorecast162
 		utils.map("n", "<Space>so", ":Vista!!<CR>")
 	end}
+
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+
+	use {'williamboman/mason.nvim', after = 'nvim-lspconfig', config = function()
+	end}
+
+	use {'williamboman/mason-lspconfig.nvim', after = 'mason.nvim', config = function()
+	end}
+
+	use {'hrsh7th/nvim-cmp', after = 'mason-lspconfig.nvim', config = function()
+		-- Set up nvim-cmp.
+		local cmp = require'cmp'
+
+		cmp.setup({
+			snippet = {
+				-- REQUIRED - you must specify a snippet engine
+				expand = function(args)
+					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+					-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+				end,
+			},
+			window = {
+				-- completion = cmp.config.window.bordered(),
+				-- documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				['<C-b>'] = cmp.mapping.scroll_docs(-4),
+				['<C-f>'] = cmp.mapping.scroll_docs(4),
+				['<C-Space>'] = cmp.mapping.complete(),
+				['<C-e>'] = cmp.mapping.abort(),
+				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			}),
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+				{ name = 'vsnip' }, -- For vsnip users.
+				-- { name = 'luasnip' }, -- For luasnip users.
+				-- { name = 'ultisnips' }, -- For ultisnips users.
+				-- { name = 'snippy' }, -- For snippy users.
+			}, {
+				{ name = 'buffer' },
+			})
+		})
+
+		-- Set configuration for specific filetype.
+		cmp.setup.filetype('gitcommit', {
+			sources = cmp.config.sources({
+				{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+			}, {
+				{ name = 'buffer' },
+			})
+		})
+
+		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline({ '/', '?' }, {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = 'buffer' }
+			}
+		})
+
+		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline(':', {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = 'path' }
+			}, {
+				{ name = 'cmdline' }
+			})
+		})
+
+		-- Set up lspconfig.
+
+		require("mason").setup()
+		require("mason-lspconfig").setup()
+
+    require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            require("lspconfig")[server_name].setup {
+				capabilities = default_capabilities
+			}
+        end,
+    }
+
+
+	end}
+
 end)
