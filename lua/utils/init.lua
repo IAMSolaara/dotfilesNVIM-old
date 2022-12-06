3fe5b3e6e5778a859cce0027b90f9ffdd37a6f24
Lorenzo Cauli <lorenzo.cauli@energit.it>
2022-12-06 12:48:34 +0100

    add OS get function to utils

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -7,4 +7,13 @@ M.map = function (mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+M.unmap = function (mode, lhs)
+    vim.api.nvim_del_keymap(mode, lhs)
+end
+
+M.get_os = function ()
+	local sysname = vim.loop.os_uname().sysname
+	return sysname
+end
+
 return M
